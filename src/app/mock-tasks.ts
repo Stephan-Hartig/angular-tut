64cda6071ff82af689f85ceaef23983c6dd5e07e
Stephan Hartig <the.stephan.hartig@gmail.com>
2021-06-28 20:21:47 -0400

    Followed tutorial up to the minimal working product

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'July 1st at 3:40pm',
+    reminder: false,
+  },
+  {
+    id: 2,
+    text: 'Lunch with Greg',
+    day: 'July 2nd at 12:00pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Surgery',
+    day: 'July 3rd at 8am',
+    reminder: false,
+  },
+];
